dce181729d153259a93958bf3db3c3f22b5c5ba2
Fernão Lopes <fernao@riseup.net>
2017-02-06 15:16:38 -0200

    Atualizando plugin mobilize

--- src/wp-content/plugins/mobilize/views/chamada.php
+++
@@ -1,9 +0,0 @@
-<div style="padding-left: {{ padding }}; padding-right: {{ padding }};">
-	<h1>{{ chamadaTitle }}</h1>
-
-	<div class="section-description">
-
-	    <p>{{ chamadaDescription }}</p>
-	    
-	</div>
-</div>

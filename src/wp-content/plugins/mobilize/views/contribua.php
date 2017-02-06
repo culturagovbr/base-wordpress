973bd85fcc1b940b944442042dddd9a21bfb7756
Fernão Lopes <fernao@riseup.net>
2017-02-06 15:16:38 -0200

    Atualizando plugin mobilize

--- src/wp-content/plugins/mobilize/views/contribua.php
+++
@@ -1,5 +0,0 @@
-<section id="mobilize-contribua" class="mobilize-widget clearfix"
-	style="padding-left: <?php echo $padding; ?>; padding-right: <?php echo $padding;?>;">
-	<h6><?php _e('Contribua', 'mobilize'); ?></h6>
-	<?php require Contribua::getContentPath(); ?>
-</section>

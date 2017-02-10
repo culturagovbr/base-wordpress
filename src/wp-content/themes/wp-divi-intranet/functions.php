a2a8fef204e6e544a033bdf972383fe82b83dbdc
Fernão Lopes <fernao@riseup.net>
2017-02-10 15:30:45 -0200

    trocando de pasta para submodulo

--- src/wp-content/themes/wp-divi-intranet/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-// adicionando css que faz a função do divi custom css (Carrega por último)
-function divi_child_enqueue_styles() {
-    wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/custom.css');
-}
-
-add_action( 'wp_head', 'divi_child_enqueue_styles' );
-
-?>

12227dd0f5d02ae36904f758b6c9ed0827f5e6c8
Fernão Lopes <fernao@riseup.net>
2017-05-31 16:04:15 -0300

    Adicionando tema wp-premiocultural

---
+++ src/wp-content/themes/wp-premiocultural/includes/social_icons.php
@@ -0,0 +1,25 @@
+
+<ul class="et-social-icons">
+
+	<li class="et-social-icon et-social-facebook">
+		<a href="https://www.facebook.com/MinisterioDaCultura/?fref=ts" class="icon" target="_blank">
+			<span>Facebook</span>
+		</a>
+	</li>
+	<li class="et-social-icon et-social-twitter">
+		<a href="https://twitter.com/culturagovbr" class="icon" target="_blank">
+			<span>Twitter</span>
+		</a>
+	</li>
+	<li class="et-social-icon et-social-youtube">
+		<a href="https://www.youtube.com/user/ministeriodacultura" class="icon" target="_blank">
+			<span>youtube</span>
+		</a>
+	</li>
+	<li class="et-social-icon et-social-rss">
+		<a href="http://rouanet.cultura.gov.br/feed/" class="icon" target="_blank">
+			<span>RSS</span>
+		</a>
+	</li>
+
+</ul>

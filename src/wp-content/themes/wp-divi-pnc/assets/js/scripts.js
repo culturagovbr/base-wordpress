28a2550ec243322109d1dc3f5c3c7484fb6d8e6a
Moises Rabelo de Lima <95274316115@minc.intra>
2017-07-10 11:02:24 -0300

    [INITIAL] first commit

---
+++ src/wp-content/themes/wp-divi-pnc/assets/js/scripts.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function ($) {
+	
+//*** Função Menu collapse responsivo*** 
+$(".menu-responsivo").on("click", function(){
+    $("#top-menu-nav").toggleClass("menu-collapse");
+});
+
+/*Leitor de rss script home*/
+$('#divRss').FeedEk({
+		FeedUrl:'http://www.cultura.gov.br/rss-backup/-/asset_publisher/PBe5d9MJmlrW/rss?p_p_cacheability=cacheLevelPage',
+		MaxCount: 5,
+		DateFormat: 'DD MMMM YYYY',
+		DateFormatLang: 'pt'
+	});
+
+ /*Leitor de rss pagina noticias*/
+    $('#rss-read').FeedEk({
+		FeedUrl:'http://www.cultura.gov.br/rss-backup/-/asset_publisher/PBe5d9MJmlrW/rss?p_p_cacheability=cacheLevelPage',
+		MaxCount: 20,
+		DateFormat: 'DD MMMM YYYY',
+		DateFormatLang: 'pt'
+	});
+
+});
---
+++ src/wp-content/themes/wp-divi-pnc/functions.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Criando uma area de widgets
+ *
+ */
+function widgets_novos_widgets_init() {
+
+    register_sidebar( array(
+        'name' => 'barra-servico',
+        'id' => 'barra-servico',
+        'before_widget' => '<div>',
+        'after_widget' => '</div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>',
+    ) );
+}
+add_action( 'widgets_init', 'widgets_novos_widgets_init' );
+?>
+
+<?php
+// adicionando css que faz a função do divi custom css (Carrega por último)
+function divi_child_enqueue_styles() {
+    // wp_enqueue_style( 'open-sans-font', 'https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' );
+    
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+        wp_enqueue_style( 'feedRead-style', get_stylesheet_directory_uri() . '/assets/css/FeedEk.css');
+    wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/assets/css/custom.css');
+    wp_enqueue_style( 'alto-contraste', get_stylesheet_directory_uri() . '/assets/css/alto-contraste.css');
+    wp_enqueue_script( 'alto-contraste', get_stylesheet_directory_uri() . '/assets/js/alto-contraste.js');
+    wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/assets/js/scripts.js' , array( 'jquery' ) );
+    wp_enqueue_script( 'feedRead', get_stylesheet_directory_uri() . '/assets/js/FeedEk.js' ,array( 'jquery' ) );
+
+    $pdaJSObj = array(
+        'ajax_url' => admin_url('admin-ajax.php'),
+        'post_id' => get_the_ID(),
+        'post_name' => get_the_title( get_the_ID() ),
+    );
+
+    wp_localize_script( 'scripts', 'pdaJSObj', $pdaJSObj );
+}
+
+add_action( 'wp_head', 'divi_child_enqueue_styles' );
+

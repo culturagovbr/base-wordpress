705388333f37514161fc51698108fb8669f216a1
Rick Manu <rickmanu@gmail.com>
2017-08-10 15:02:09 -0300

    [UPDATE] Atualizacao das marcas no rodape

--- src/wp-content/themes/wp-inscricao-oscar/footer.php
+++ src/wp-content/themes/wp-inscricao-oscar/footer.php
@@ -2,13 +2,13 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-secretaria-do-audiovisual-05.png">
+			</div>
+			<div class="col-md-4">
 				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-ministerio-da-cultura.png">
 			</div>
 			<div class="col-md-4">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-governo-federal.png">
-			</div>
-			<div class="col-md-4">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/sav.png">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-brasil.png">
 			</div>
 		</div>
 	</div>

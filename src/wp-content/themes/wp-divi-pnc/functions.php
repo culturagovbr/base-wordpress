8c01a012220d0e66e98de3c584446ef8ecdecf95
Moises Rabelo de Lima <95274316115@minc.intra>
2017-08-28 17:15:02 -0300

    [Update] melhoria no plugin de compartilhamento

--- src/wp-content/themes/wp-divi-pnc/functions.php
+++ src/wp-content/themes/wp-divi-pnc/functions.php
@@ -43,7 +43,22 @@ function divi_child_enqueue_styles() {
 
 add_action( 'wp_head', 'divi_child_enqueue_styles' );
 
+//Adicionando função que criar pege url
 
+add_shortcode ('geturl', 'get_current_page_url');
+function get_current_page_url() {
+    $pageURL = 'http';
+    if( isset($_SERVER["HTTPS"]) ) {
+        if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+    }
+    $pageURL .= "://";
+    if ($_SERVER["SERVER_PORT"] != "80") {
+        $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+    } else {
+        $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+    }
+    return $pageURL;
+}
 
 
 // Adicionando a função que cria o Breadcrumbs

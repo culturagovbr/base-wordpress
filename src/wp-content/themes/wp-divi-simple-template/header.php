a72c98e55da6bde184edfcec794da35efdf91105
Rick Manu <rickmanu@gmail.com>
2017-09-12 14:43:54 -0300

    [ADD] Configuracoes para o template, com opcoes de troca de cores padroes e mais

--- src/wp-content/themes/wp-divi-simple-template/header.php
+++ src/wp-content/themes/wp-divi-simple-template/header.php
@@ -32,13 +32,13 @@
 
     <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
+<body <?php body_class( ( get_minc_option('theme_color') ) ? 'tema-' . get_minc_option('theme_color') : ''  ); ?>>
 <div id="header" role="banner">
     <div class="clearfix">
         <ul id="accessibility">
             <li>
                 <a accesskey="1" href="#div-conteudo" id="link-conteudo">
-                    Ir para o conte&uacute;do
+                    Ir para o conteúdo
                     <span>1</span>
                 </a>
             </li>
@@ -56,7 +56,7 @@
             </li>
             <li>
                 <a accesskey="4" href="#main-footer" id="link-rodape">
-                    Ir para o rodap&eacute;
+                    Ir para o rodapé
                     <span>4</span>
                 </a>
             </li>
@@ -75,24 +75,42 @@
         </ul>
 
         <div id="logo-header-tema">
-            <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>">
-                <div id="portal-title"><?php bloginfo('name'); ?></div>
-                <div id="portal-description" style="color:#FFF">
-                    <?php bloginfo('description'); ?>
-                </div>
-            </a>
+            <h1 id="portal-title">
+                <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>">
+                    <?php bloginfo('name'); ?>
+                </a>
+            </h1>
+            <p id="portal-description">
+                <?php bloginfo('description'); ?>
+            </p>
         </div>
 
+        <?php if( get_minc_option('show_searchbar') ): ?>
+        <div id="portal-searchbox" role="search">
+            <form role="search" method="get" class="" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <?php
+                    printf( '<input type="search" class="et-search-field"  accesskey="3" placeholder="%1$s" value="%2$s" name="s" title="%3$s" /> <input type="submit" value="Buscar" class="searchButton">',
+                        esc_attr__( 'Search &hellip;', 'Divi' ),
+                        get_search_query(),
+                        esc_attr__( 'Buscar:', 'Divi' )
+                    );
+                ?>
+            </form>
+        </div>
+        <?php endif; ?>
+
+        <?php if( get_minc_option('show_social_links') ): ?>
         <div id="social-icons">
             <ul>
-                <!-- <li>
+                <li>
                     <a href="#!" title="Github" target="_blank" class="github">
                         <span>Github</span>
                         <svg aria-hidden="true" class="octicon octicon-mark-github" height="18" version="1.1" viewBox="0 0 16 16" width="18"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
                     </a>
-                </li> -->
+                </li>
             </ul>
         </div>
+        <?php endif; ?>
 
     </div>
     <?php

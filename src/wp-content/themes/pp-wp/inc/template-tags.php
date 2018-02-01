eb6eaa92ecb97b29891f2a46c1705d3cb354e918
Rick Manu <rickmanu@gmail.com>
2018-02-01 11:39:31 -0200

    [UPDATE] Template padrao: Ajuste na lista de posts - removendo o campo de comentarios, quando a opcao estiver desabilitada

--- src/wp-content/themes/pp-wp/inc/template-tags.php
+++ src/wp-content/themes/pp-wp/inc/template-tags.php
@@ -57,7 +57,8 @@ if (!function_exists ('pp_wp_entry_footer')) :
 			}
 		}
 
-		if (!is_single () && !post_password_required () && (comments_open () || get_comments_number ())) {
+		$pp_theme_options_options = get_option( 'pp_theme_options_option_name' );
+		if (!is_single () && !post_password_required () && (comments_open () || get_comments_number ()) && !$pp_theme_options_options['disable_comments']) {
 			echo '<span class="comments-link">';
 			comments_popup_link (sprintf (wp_kses (
 				__ ('Leave a Comment<span class="screen-reader-text"> on %s</span>', 'pp-wp'), array('span' => array('class' => array(),),)), get_the_title ()));

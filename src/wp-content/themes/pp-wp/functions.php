0217690ca764812ece69e2659cbb21205741ce86
Rick Manu <rickmanu@gmail.com>
2018-02-19 09:28:45 -0300

    [ADD] Tema padrao: Adicao de shortcodes

--- src/wp-content/themes/pp-wp/functions.php
+++ src/wp-content/themes/pp-wp/functions.php
@@ -171,6 +171,12 @@ require_once get_template_directory() . '/inc/wp-bootstrap-navwalker.php';
 require_once get_template_directory() . '/inc/breadcrumb.php';
 
 /**
+ * Shortcodes
+ *
+ */
+require_once get_template_directory() . '/inc/shortcodes.php';
+
+/**
  * Corrects the error while customizing the site in the frontend
  *
  */

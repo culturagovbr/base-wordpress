317f69e5baf6e5195251e550032c7ed5c17c43f8
Ricardo Carvalho <rickmanu@gmail.com>
2018-04-24 16:18:51 -0300

    [ADD] Plugin Get Site Header: Plugin para retornar o cabecalho do site

---
+++ src/wp-content/plugins/get-site-header/get-site-header.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Name:       Get Site Header
+ * Plugin URI:        https://github.com/culturagovbr/
+ * Description:       Retorna o cabeçalho do site para ser usado de forma dinamica em outros locais
+ * Version:           1.0.0
+ * Author:            Ricardo Carvalho
+ * Author URI:        https://github.com/darciro/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+if ( ! class_exists( 'GetSiteHeader' ) ) :
+
+	class GetSiteHeader {
+
+		public function __construct() {
+            add_action( 'wp_ajax_nopriv_get_header', array($this, 'get_header'));
+            add_action( 'wp_ajax_get_header', array($this, 'get_header'));
+		}
+
+		public function get_header () {
+            get_header();
+            die;
+		}
+
+    }
+
+	// Initialize our plugin
+	$gewp = new GetSiteHeader();
+
+endif;

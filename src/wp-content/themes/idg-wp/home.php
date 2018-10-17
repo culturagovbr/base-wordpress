7077a72df5c271a78c0443ac913a19f3606e42e2
Daniel Moura <daniel.mouraa@gmail.com>
2018-10-17 18:52:41 -0300

    [UPDATE] Tema IDG - Alinhamento Home

--- src/wp-content/themes/idg-wp/home.php
+++ src/wp-content/themes/idg-wp/home.php
@@ -74,58 +74,58 @@ get_header();
 						<div class="container">
 								<div class="row">
 										<div class="col-lg-12">
-											<h2 class="section-title mb-4 text-center">Serviços</h2>
+											<h2 class="section-title mb-5 text-center">Serviços</h2>
 										</div>
-									<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+									<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 										<div class="feature-card icon-law">
 							<h3 class="card-title">Lei Rouanet</h3>
 											<p class="card-desc">Lorem ipsum dolor sit amet, consetetur sadipscing elitr</p>
 											<a class="card-btn btn" href="#">Acesse</a>
 										</div>
 									</div>
-									<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+									<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 										<div class="feature-card icon-edicts">
 											<h3 class="card-title">Editais</h3>
 											<p class="card-desc">Lorem ipsum dolor sit amet, consetetur sadipscing elitr</p>
 											<a class="card-btn btn" href="#">Acesse</a>
 										</div>
 									</div>
-									<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+									<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 										<div class="feature-card icon-rocket">
 											<h3 class="card-title">Apoio à projetos</h3>
 											<p class="card-desc">Lorem ipsum dolor sit amet, consetetur sadipscing elitr</p>
 											<a class="card-btn btn" href="#">Acesse</a>
 										</div>
 									</div>
-									<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+									<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 										<div class="feature-card icon-patrimony">
 											<h3 class="card-title">Patrimônio</h3>
 											<p class="card-desc">Lorem ipsum dolor sit amet, consetetur sadipscing elitr</p>
 											<a class="card-btn btn" href="#">Acesse</a>
 										</div>
 									</div>
-									<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+									<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 										<div class="feature-card icon-culture-center">
 											<h3 class="card-title">Centros culturais</h3>
 											<p class="card-desc">Lorem ipsum dolor sit amet, consetetur sadipscing elitr</p>
 											<a class="card-btn btn" href="#">Acesse</a>
 										</div>
 									</div>
-									<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+									<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 										<div class="feature-card icon-culture-points">
 											<h3 class="card-title">Pontos de cultura</h3>
 											<p class="card-desc">Lorem ipsum dolor sit amet, consetetur sadipscing elitr</p>
 											<a class="card-btn btn" href="#">Acesse</a>
 										</div>
 									</div>
-									<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+									<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 										<div class="feature-card icon-flag">
 											<h3 class="card-title">SNC</h3>
 											<p class="card-desc">Lorem ipsum dolor sit amet, consetetur sadipscing elitr</p>
 											<a class="card-btn btn" href="#">Acesse</a>
 										</div>
 									</div>
-									<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+									<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 										<div class="feature-card icon-creative">
 											<h3 class="card-title">Economia Criativa</h3>
 											<p class="card-desc">Lorem ipsum dolor sit amet, consetetur sadipscing elitr</p>
@@ -138,13 +138,11 @@ get_header();
 								</div>
 						</div>
 				</section>
-
-<<<<<<< HEAD
 				<section class="mt-5 mb-5">
 						<div class="container">
 								<div class="row">
 									<div class="col-lg-12">
-										<h2 class="section-title mb-4 text-center">Notícias</h2>
+										<h2 class="section-title mb-5 text-center">Notícias</h2>
 									</div>
 									<div class="col-lg-4">
 										<div class="highlight-box">
@@ -190,7 +188,7 @@ get_header();
 						<div class="container">
 								<div class="row">
 									<div class="col-lg-12">
-										<h2 class="section-title mb-4 text-center text-uppercase" style="color: #fff; font-size: 24px">Agenda Cultural</h2>
+										<h2 class="section-title mb-5 text-center text-uppercase" style="color: #fff; font-size: 24px">Agenda Cultural</h2>
 									</div>
 								</div>
 						</div>
@@ -200,44 +198,44 @@ get_header();
 						<div class="container">
 							<div class="row">
 								<div class="col-lg-12">
-									<h2 class="section-title mb-4 text-center" style="color: #2D5A92">Conheça o ministério</h2>
+									<h2 class="section-title mb-5 text-center" style="color: #2D5A92">Conheça o ministério</h2>
 								</div>
-								<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+								<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 									<div class="feature-card card-2 icon-minister">
 										<h3 class="card-title"><a href="#">O Ministro</a></h3>
 									</div>
 								</div>
-								<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+								<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 									<div class="feature-card card-2 icon-ministry">
 										<h3 class="card-title"><a href="#">O Ministério</a></h3>
 									</div>
 								</div>
-								<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+								<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 									<div class="feature-card card-2 icon-secretaries">
 										<h3 class="card-title"><a href="#">Secretarias</a></h3>
 									</div>
 								</div>
-								<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+								<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 									<div class="feature-card card-2 icon-linkeds">
 										<h3 class="card-title"><a href="#">Vinculadas</a></h3>
 									</div>
 								</div>
-								<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+								<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 									<div class="feature-card card-2 icon-internacional">
 										<h3 class="card-title"><a href="#">Internacional</a></h3>
 									</div>
 								</div>
-								<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+								<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 									<div class="feature-card card-2 icon-actions">
 										<h3 class="card-title"><a href="#">Ações programas</a></h3>
 									</div>
 								</div>
-								<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+								<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 									<div class="feature-card card-2 icon-legislation">
 										<h3 class="card-title"><a href="#">Legislação</a></h3>
 									</div>
 								</div>
-								<div class="col-sm-12 col-md-6 col-lg-3 mb-4 text-center">
+								<div class="col-sm-12 col-md-6 col-lg-3 text-center">
 									<div class="feature-card card-2 icon-officers">
 										<h3 class="card-title"><a href="#">Escritórios regionais</a></h3>
 									</div>
@@ -250,24 +248,24 @@ get_header();
 						<div class="container">
 								<div class="row">
 									<div class="col-lg-12">
-										<h2 class="section-title mb-4 text-center">Conteúdos</h2>
+										<h2 class="section-title mb-5 text-center">Conteúdos</h2>
 									</div>
-									<div class="col-md-6 col-lg-3 mb-4 text-center">
+									<div class="col-md-6 col-lg-3 text-center">
 										<div class="feature-card card-2 icon-advisory">
 											<h3 class="card-title"><a href="#">Assessoria</a></h3>
 										</div>
 									</div>
-									<div class="col-md-6 col-lg-3 mb-4 text-center">
+									<div class="col-md-6 col-lg-3 text-center">
 										<div class="feature-card card-2 icon-news">
 											<h3 class="card-title"><a href="#">Notícias</a></h3>
 										</div>
 									</div>
-									<div class="col-md-6 col-lg-3 mb-4 text-center">
+									<div class="col-md-6 col-lg-3 text-center">
 										<div class="feature-card card-2 icon-publications">
 											<h3 class="card-title"><a href="#">Publicações</a></h3>
 										</div>
 									</div>
-									<div class="col-md-6 col-lg-3 mb-4 text-center">
+									<div class="col-md-6 col-lg-3 text-center">
 										<div class="feature-card card-2 icon-multimedia">
 											<h3 class="card-title"><a href="#">Multimídia</a></h3>
 										</div>
@@ -280,7 +278,7 @@ get_header();
 				<div class="container">
 					<div class="row">
 						<div class="col-lg-12">
-							<h2 class="section-title mb-4 text-center">Participação Social</h2>
+							<h2 class="section-title mb-5 text-center">Participação Social</h2>
 						</div>
 						<div class="col-lg-3 mb-4 text-center">
 							<div class="feature-card icon-talk-to-us">

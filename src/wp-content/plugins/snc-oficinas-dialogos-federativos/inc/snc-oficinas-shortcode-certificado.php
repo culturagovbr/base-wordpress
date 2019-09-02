f1f1c4daf40cb68b57d23317efb08cd7cbe20e33
Janilson <janilson.mendes@gmail.com>
2019-09-02 17:35:52 -0300

    Ajustes.

---
+++ src/wp-content/plugins/snc-oficinas-dialogos-federativos/inc/snc-oficinas-shortcode-certificado.php
@@ -0,0 +1,153 @@
+<?php
+
+if (!defined('WPINC'))
+    die();
+
+require_once SNC_ODF_PLUGIN_PATH . '/vendor/autoload.php';
+
+class SNC_Oficinas_Shortcode_Certificado
+{
+    private $configPdf = [
+        'mode' => 'utf-8',
+        'default_font_size' => 0,
+        'default_font' => '',
+        'margin_left' => 0,
+        'margin_right' => 0,
+        'margin_top' => 0,
+        'margin_bottom' => 0,
+        'margin_header' => 0,
+        'margin_footer' => 0,
+        'orientation' => 'L',
+    ];
+
+    private $mpdf;
+
+    private $idOficina = null;
+
+    public function __construct($idOficina = null)
+    {
+        if (!is_admin()) {
+            add_shortcode('snc-certificado', array($this, 'snc_impressao'));
+        }
+
+        $this->configPdf['tempDir'] = get_temp_dir();
+
+        $this->mpdf = new Mpdf\Mpdf($this->configPdf);
+
+        add_filter('page_template', array($this, 'snc_oficinas_page_template'));
+
+        $this->idOficina = isset($_GET['idOficina']) ? $_GET['idOficina'] : $idOficina;
+    }
+
+    public function snc_impressao()
+    {
+        $this->_generatePdf();
+
+        $this->mpdf->Output('certificado_oficinas_snc.pdf', \Mpdf\Output\Destination::DOWNLOAD);
+    }
+
+    public function uploadPdf()
+    {
+        $this->_generatePdf();
+
+        $uploadDir = wp_upload_dir();
+
+        $local = "{$uploadDir['path']}/certificado_oficinas_snc_" . time() . ".pdf";
+
+        $this->mpdf->Output($local, \Mpdf\Output\Destination::FILE);
+
+        return $local;
+    }
+
+    private function _generatePdf()
+    {
+        if (!is_user_logged_in()) {
+            echo "Autenticação é obrigatória para acessar este recurso";
+            return false;
+        }
+
+        global $texto;
+        global $textoData;
+        global $horas;
+        global $url;
+        global $unidade;
+        global $prefixo;
+
+        $current_user = wp_get_current_user();
+        $name = $current_user->display_name;
+
+        $oficina = get_fields($this->idOficina);
+        $oficina_campos = get_fields($oficina['inscricao_oficina_uf']->ID);
+
+        $uf = $oficina_campos['oficina_unidade_da_federacao'];
+
+        $unidade = mb_substr($uf, 0, strpos($uf, " ("), "UTF-8");
+        $sigla = mb_substr($uf, strpos($uf, "(") + 1, 2, "UTF-8");
+        $prefixo = $this->_prefixo($sigla);
+
+        $dataInicio = explode("/", $oficina_campos['oficina_data_inicio']);
+        $dataFinal = explode("/", $oficina_campos['oficina_data_final']);
+
+        $objDateIni = new DateTime("{$dataInicio[2]}-{$dataInicio[1]}-{$dataInicio[0]}");
+        $objDateFinal = new DateTime("{$dataFinal[2]}-{$dataFinal[1]}-{$dataFinal[0]}");
+
+        $dias = $objDateFinal->diff($objDateIni)->days + 1;
+        $horas = ($oficina_campos['oficina_horario_termino'] - $oficina_campos['oficina_horario_inicio']) * $dias;
+
+
+        $texto = "Certificamos que {$name} participou do evento “<b>Diálogos Federativos: Cultura de Ponto à Ponta</b>”";
+        $texto .= ", realizado pela Secretaria da Diversidade Cultural em parceria com o Estado {$prefixo} ";
+        $texto .= " {$unidade}, no período de {$oficina_campos['oficina_data_inicio']}";
+        $texto .= " à {$oficina_campos['oficina_data_final']}, com carga horária total de {$horas} horas.";
+
+        $mes = $this->_getMes((int)$dataFinal[1]);
+        $textoData = "Brasília, {$dataFinal[0]} de {$mes} de $dataFinal[2].";
+        $url = SNC_ODF_PLUGIN_PATH . "assets/base_cert.png";
+
+        ob_start();
+
+        require_once SNC_ODF_PLUGIN_PATH . '/pages/certificado-pdf.php';
+        $template = ob_get_contents();
+
+        ob_end_clean();
+
+        $this->mpdf->WriteHTML($template);
+    }
+
+    private function _getMes($mes)
+    {
+        $meses = array(1 => 'Janeiro', 2 => 'Fevereiro', 3 => 'Março', 4 => 'Abril', 5 => 'Maio', 6 => 'Junho',
+            7 => 'Julho', 8 => 'Agosto', 9 => 'Setembro', 10 => 'Outubro', 11 => 'Novembro', 12 => 'Dezembro');
+
+        return $meses[$mes];
+    }
+
+    private function _prefixo($sigla)
+    {
+        $prefixoA = ['BA', 'PB'];
+        $prefixoE = ['AL', 'MG', 'PE', 'RO', 'RR', 'SC', 'SE', 'SP'];
+        $prefixoO = ['AC', 'AP', 'AM', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'PA', 'PI', 'PR', 'RJ', 'RN', 'RS', 'TO'];
+
+        if (in_array($sigla, $prefixoA)) {
+            return 'da';
+        }
+
+        if (in_array($sigla, $prefixoE)) {
+            return 'de';
+        }
+
+        if (in_array($sigla, $prefixoO)) {
+            return 'do';
+        }
+
+        return '';
+    }
+
+    public function snc_oficinas_page_template($page_template)
+    {
+        if (is_page('certificado')) {
+            $page_template = SNC_ODF_PLUGIN_PATH . '/pages/custom-page-template.php';
+            return $page_template;
+        }
+    }
+}
---
+++ src/wp-content/plugins/snc-oficinas-dialogos-federativos/inc/snc-oficinas-shortcode-formulario-participacao.php
@@ -0,0 +1,211 @@
+<?php
+
+if (!defined('WPINC'))
+    die();
+
+class SNC_Oficinas_Shortcode_Formulario_Participacao
+{
+    public function __construct()
+    {
+        if (!is_admin()) {
+            add_action('get_header', array($this, 'add_acf_form_head'), 0);
+            add_shortcode('snc-oficina-response-questions', array($this, 'snc_response_questions_form'));
+            add_action('acf/pre_save_post', array($this, 'preprocess_main_form'));
+            add_action('acf/save_post', array($this, 'postprocess_main_form_update'));
+        }
+    }
+
+    public function snc_response_questions_form($atts)
+    {
+        $token = esc_attr($_GET['token']);
+        $subscription_id = esc_attr($_GET['id']);
+
+        try {
+            ob_start();
+
+            if (!SNC_Oficinas_Validator::is_token_valid($subscription_id, $token, 'token_responder_questionario')) {
+                throw new Exception("Erro! Os dados informados são inválidos.");
+            }
+
+            $inscricaoPerfil = get_post_meta($subscription_id, 'inscricao_perfil', true);
+
+            $questionarioUrl = "questionario";
+
+            switch ($inscricaoPerfil) {
+                case 'Gestor de Cultura':
+                    $questionarioUrl .= "-gestor";
+                    break;
+                case 'Conselheiro de Cultura':
+                    $questionarioUrl .= "-conselheiro";
+                    break;
+                case 'Ponteiro de Cultura':
+                    $questionarioUrl .= "-ponteiro";
+                    break;
+                default:
+                    $questionarioUrl .= "";
+                    break;
+            }
+
+            $url = home_url("/{$questionarioUrl}/?token={$token}&id={$subscription_id}&sent=true#message");
+
+            $atts = shortcode_atts(array(
+                'form-group-id' => '',
+                'return' => $url
+            ), $atts);
+
+            $settings = array(
+                'field_groups' => array($atts['form-group-id']),
+                'id' => 'snc-question-form',
+                'post_id' => SNC_POST_TYPE_PARTICIPACAO,
+                'new_post' => array(
+                    'post_type' => SNC_POST_TYPE_PARTICIPACAO,
+                    'post_status' => 'publish'
+                ),
+                'updated_message' => 'Questionário enviado com sucesso.',
+                'return' => home_url("/inscricoes/?status=finalizado&id={$subscription_id}"),
+                'uploader' => 'basic',
+                'submit_value' => 'Enviar'
+            );
+
+            $status = $_GET['status'];
+
+            $subscription = $this->get_subscription_in_workshop();
+            if (count($subscription) == 0) {
+                acf_form($settings);
+            } else {
+                $this->get_message_user_registered();
+            }
+        } catch (Exception $e) {
+            $this->_get_message_subscription_error($e->getMessage());
+        } finally {
+            return ob_get_clean();
+        }
+    }
+
+    private function _get_message_subscription_error($message)
+    {
+        ?>
+        <div class="container-fluid">
+            <div class="alert alert-danger" role="alert">
+                <?= $message; ?>
+                <a href="<?= home_url('/inscricoes/') ?>">Clique aqui para consultar o status da inscrição</a>
+            </div>
+        </div>
+        <?php
+    }
+
+    private function get_message_register_success()
+    {
+        ?>
+        <div class="alert alert-success" role="alert">
+            Usuário cadastrado com sucesso! Preencha os dados abaixo para finalizar a solicitação de inscrição na
+            oficina!
+        </div>
+        <?php
+    }
+
+    private function get_message_subscription_pending($subscription)
+    {
+        ?>
+        <div style="margin-bottom: 100px">
+            <div class="alert alert-success" role="alert">
+                Solicitação de inscrição enviada com sucesso. Confirme seu e-mail para prosseguir com a inscrição.
+            </div>
+
+            Se desejar,
+            <a href="<?= home_url('/inscricoes/') ?>">clique aqui para acompanhar o status da inscrição</a>
+        </div>
+        <?php
+    }
+
+    private function get_subscription_in_workshop()
+    {
+        if (is_user_logged_in()) {
+            $post = get_posts([
+                'author' => get_current_user_id(),
+                'post_type' => SNC_POST_TYPE_PARTICIPACAO,
+                'post_status' => array('waiting_questions'),
+                'posts_per_page' => 1
+            ]);
+
+            return $post;
+        }
+
+        return false;
+    }
+
+    public function add_acf_form_head()
+    {
+        if (shortcode_exists('snc-question-form')) {
+            acf_form_head();
+        }
+    }
+
+    /**
+     * Notify the monitors about a new subscription
+     *
+     * @param $post_id
+     */
+    public function postprocess_main_form_update($post_id)
+    {
+        $subscription_id = esc_attr($_GET['id']);
+
+        $subscription = array('ID' => $subscription_id, 'post_status' => 'finish');
+
+
+
+        wp_update_post($subscription);
+
+        delete_post_meta($subscription_id, 'token_responder_questionario');
+
+        $sncEmail = new SNC_Oficinas_Email($subscription_id, 'snc_email_impress_cert');
+        $sncEmail->snc_send_mail_user();
+    }
+
+    /**
+     * Process data before save indication post
+     *
+     * @param $post_id
+     * @return int|void|WP_Error
+     */
+    public function preprocess_main_form($post_id)
+    {
+        $subscription_id = esc_attr($_GET['id']);
+
+        if ($post_id != SNC_POST_TYPE_PARTICIPACAO) {
+            return $post_id;
+        }
+
+        if (is_admin()) {
+            return;
+        }
+
+        $post = array('post_type' => SNC_POST_TYPE_PARTICIPACAO, 'post_status' => 'publish');
+        $post_id = wp_insert_post($post);
+
+        $questionario = array('ID' => $post_id,
+            'post_title' => 'Questionário - (ID #' . $post_id . ')',
+            'post_parent' => $subscription_id);
+
+        wp_update_post($questionario);
+
+        return $post_id;
+    }
+
+    function snc_filter_workshops($args)
+    {
+        $args['post_status'] = 'confirmed';
+        return $args;
+    }
+
+    function snc_filter_workshops_object_result($title, $post, $field, $post_id)
+    {
+        $fields = get_fields($post->ID, true);
+        $data_inicio = $fields['oficina_data_inicio'];
+        $data_final = $fields['oficina_data_final'];
+        $local = $fields['oficina_loca_de_realizacao'];
+        $title = "<b>$title</b> Data:  $data_inicio a $data_final Local $local ";
+
+        return $title;
+    }
+}

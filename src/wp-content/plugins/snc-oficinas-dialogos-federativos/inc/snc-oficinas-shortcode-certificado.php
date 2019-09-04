e4dcfa795b7de0d10c175af682228633e23a4d2f
Janilson <janilson.mendes@gmail.com>
2019-09-04 14:24:17 -0300

    Ajustes.

--- src/wp-content/plugins/snc-oficinas-dialogos-federativos/inc/snc-oficinas-shortcode-certificado.php
+++ src/wp-content/plugins/snc-oficinas-dialogos-federativos/inc/snc-oficinas-shortcode-certificado.php
@@ -32,9 +32,9 @@ class SNC_Oficinas_Shortcode_Certificado
 
         add_filter('page_template', array($this, 'snc_oficinas_page_template'));
 
-        $uploadDir = wp_upload_dir();
+        $this->configPdf['tempDir'] = get_temp_dir();
 
-        $this->configPdf['tempDir'] = $uploadDir['path'];
+//        chmod ($this->configPdf['tempDir'], 0777);
 
         $this->mpdf = new Mpdf\Mpdf($this->configPdf);
 
@@ -55,6 +55,8 @@ class SNC_Oficinas_Shortcode_Certificado
         $this->_generatePdf();
 
         $uploadDir = wp_upload_dir();
+
+        chmod ($uploadDir['path'], 0777);
 
         $local = "{$uploadDir['path']}/certificado_oficinas_snc_" . time() . ".pdf";
 
